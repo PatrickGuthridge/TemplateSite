835808d2c88d040d197ceeae40be674b659d1170
QWERTYUIOPYOZO <qwertyuiop1andonly@outlook.com>
2018-01-20 10:16:47 +1100

    Fixes

--- sys.js
+++ sys.js
@@ -1,12 +1,21 @@
+var page;
 function Page(e){
-    this = document.createElement("div");
-    this.id = e;
-    this.class = "page";
-    var header = document.createElement("div");
-    header.class = "header";
-    
+    this.setup = document.createElement("div");
+    this.setup.id = e;
+    this.setup.className = "page";
+        var header = document.createElement("div");
+        header.className = "header";
+    this.setup.appendChild(header);
+        var content = document.createElement("div");
+        content.className = "content";
+    this.setup.appendChild(content);
+        var footer = document.createElement("div");
+        footer.className = "footer";
+    this.setup.appendChild(footer);
 }
 function init(e){
-    var page = new Page("home");
+    page = new Page("home");
+    document.body.innerHTML = "";
+    document.body.appendChild(page.setup);
 }
 window.addEventListener("load", init);
