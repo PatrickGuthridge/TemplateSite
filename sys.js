3ef699e62f6d183f81e2fbe6fafbf09e5ea48d09
QWERTYUIOPYOZO <qwertyuiop1andonly@outlook.com>
2018-01-20 19:15:00 +1100

    x

--- sys.js
+++ sys.js
@@ -1,21 +1,52 @@
-var page;
-function Page(e){
-    this.setup = document.createElement("div");
-    this.setup.id = e;
-    this.setup.className = "page";
-        var header = document.createElement("div");
-        header.className = "header";
-    this.setup.appendChild(header);
-        var content = document.createElement("div");
-        content.className = "content";
-    this.setup.appendChild(content);
-        var footer = document.createElement("div");
-        footer.className = "footer";
-    this.setup.appendChild(footer);
+var header = {};
+var page = {};
+var footer = {};
+document.scrub = function(e){
+    var element;
+    if(!e){
+        element = document.body;
+    }
+    else{
+        element = document.querySelector(e);
+    }
+    try{
+        while(element.firstChild){
+            element.removeChild(element.firstChild);
+        }
+    }
+    catch(e){
+        console.error("Scrub error: Null selector reference,", e.message)
+    }
 }
+function Header(){
+    var setup = document.createElement("div");
+    setup.className = "header";
+
+    this.setup = setup;
+}
+function Page(){
+    var setup = document.createElement("div");
+    setup.className = "page";
+
+    this.setup = setup;
+}
+function Footer(){
+    var setup = document.createElement("div");
+    setup.className = "footer";
+
+    this.setup = setup;
+}
+
 function init(e){
-    page = new Page("home");
-    document.body.innerHTML = "";
-    document.body.appendChild(page.setup);
+    //Create main document elements
+    header = new Header();
+    page = new Page();
+    footer = new Footer();
+
+    //Clear existing document
+    document.scrub();
+    document.body.classList.remove("init");
+
+    //document.body.appendChild(header.setup);
 }
 window.addEventListener("load", init);
